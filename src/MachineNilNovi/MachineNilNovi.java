5eafe73b02b0ba3cba36d5e3499a2dbd3fb8a9db
SSonido <epalamour@gmail.com>
2020-06-02 18:15:01 +0200

    modif erwan

--- src/MachineNilNovi/MachineNilNovi.java
+++ src/MachineNilNovi/MachineNilNovi.java
@@ -2,110 +2,138 @@ package MachineNilNovi;
 
 public class MachineNilNovi {
     private Lecteur lecteur;
-    private Stack<Integer> maPile;
     private int ip;
+    private ArrayList<Integer> maPile;
     private ArrayList registres;
     private Arraylist TableIdentificateur;
+    private boolean fin;
 
     public void debutProg(){
         maPile = new Stack<Integer>();
         registres = new ArrayList<>();
+        this.fin = false;
     }
 
     public void finProg(){
-
+        this.fin = true;
     }
 
     public void reserver(int n){
-        ip = n-1;
+        if (!fin){
+            if (n >= 0) ip = n-1;
+        }
     }
 
     public void empiler(int val){
-
+        if (!fin){
+            maPile[ip] = val;
+            ip++;
+        }
     }
 
     public void affectation (){
-
+        if (!fin){
+        }
     }
 
     public void valeurPile(){
-
+        if (!fin){
+        }
     }
 
     public void get(){
-
+        if (!fin){
+        }
     }
 
     public void put(){
-
+        if (!fin){
+        }
     }
 
     public void moins(){
-
+        if (!fin){
+        }
     }
 
     public void sous(){
-
+        if (!fin){
+        }
     }
 
     public void add(){
-
+        if (!fin){
+        }
     }
 
     public void mult(){
-
+        if (!fin){
+        }
     }
 
     public void div(){
-
+        if (!fin){
+        }
     }
 
     public void equal(){
-
+        if (!fin){
+        }
     }
 
     public void diff(){
-
+        if (!fin){
+        }
     }
 
     public void inf(){
-
+        if (!fin){
+        }
     }
 
     public void infeg(){
-
+        if (!fin){
+        }
     }
 
     public void sup(){
-
+        if (!fin){
+        }
     }
 
     public void supeg(){
-
+        if (!fin){
+        }
     }
 
     public void et(){
-
+        if (!fin){
+        }
     }
 
     public void ou(){
-
+        if (!fin){
+        }
     }
 
     public void non(){
-
+        if (!fin){
+        }
     }
 
     public void tra(int ad){
-
+        if (!fin){
+        }
     }
 
     public void tze(int ad){
-
+        if (!fin){
+        }
     }
 
     public void erreur(){
-
+        if (!fin){
+        }
     }
 
 }
