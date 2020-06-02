ea2621e34cb57a4cff57736773fdbc4ee79e30ba
SSonido <epalamour@gmail.com>
2020-06-02 19:01:33 +0200

    modif erwan

--- src/MachineNilNovi/MachineNilNovi.java
+++ src/MachineNilNovi/MachineNilNovi.java
@@ -37,6 +37,9 @@ public class MachineNilNovi {
 
     public void affectation (){
         if (!fin){
+            Integer i = maPile.get(ip);
+            Integer j = maPile.get(ip - 1);
+            if (j < ip) maPile.set(j, i);
         }
     }
 
@@ -52,91 +55,169 @@ public class MachineNilNovi {
 
     public void put(){
         if (!fin){
+            System.out.println(maPile.get(ip));
         }
     }
 
     public void moins(){
         if (!fin){
+            maPile.set(ip, -maPile.get(ip))
         }
     }
 
     public void sous(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            maPile.set(ip, maPile.get(ip) - i);
         }
     }
 
     public void add(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            maPile.set(ip, maPile.get(ip) + i);
         }
     }
 
     public void mult(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            maPile.set(ip, maPile.get(ip) * i);
         }
     }
 
     public void div(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            maPile.set(ip, maPile.get(ip) / i);
         }
     }
 
     public void equal(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) == i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void diff(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) != i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void inf(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) < i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void infeg(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) <= i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void sup(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) > i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void supeg(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            maPile.remove(ip);
+            ip --;
+            if (maPile.get(ip) >= i) maPile.add(1);
+            else maPile.add(0);
+            ip++;
         }
     }
 
     public void et(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            Integer j = maPile.get(ip - 1);
+            if ((i == 0 || i == 1) && (j == 0 || j == 1)) {
+                maPile.remove(ip);
+                ip--;
+                if (j == 1 && i == 1) maPile.add(1);
+                else maPile.add(0);
+                ip++;
+            }
         }
     }
 
     public void ou(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            Integer j = maPile.get(ip - 1);
+            if ((i == 0 || i == 1) && (j == 0 || j == 1)) {
+                maPile.remove(ip);
+                ip--;
+                if (j == 1 || i == 1) maPile.add(1);
+                else maPile.add(0);
+                ip++;
+            }
         }
     }
 
     public void non(){
         if (!fin){
+            Integer i = maPile.get(ip);
+            if (i == 0)maPile.set(ip, 1);
+            if (i == 1)maPile.set(ip, 0);
         }
     }
 
     public void tra(int ad){
         if (!fin){
+            ip = ad;
         }
     }
 
     public void tze(int ad){
         if (!fin){
+            if (maPile.get(ip) == 0) ip = ad;
         }
     }
 
-    public void erreur(){
+    public void erreur(String s){
         if (!fin){
+            System.out.println(s);
         }
     }
 
